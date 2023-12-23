f94d6873f6e4de0907e18e22b385437e41c1b66c
Vinaysohaliya <sohaliyavin01@gmail.com>
2023-12-23 09:56:12 +0530

    auth appwrite added

---
+++ src/Appwrite/auth.js
@@ -0,0 +1,57 @@
+import { Account, ID } from 'appwrite'
+import config from './config/config';
+
+export class Auth{
+     client = new Client();
+     account;
+     constructor() {
+        this.client.setEndpoint(config.appWriteUrl);
+        this.client.setProject(config.appWriteProject);
+        this.account=new Account(this.client);
+     }
+
+     async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                console.log("sign ok");
+                return this.login({email, password});
+            } else {
+               return  userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({ email
+        , password }) {
+        try {
+            console.log("login ko");
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getuser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+    async logout(){
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+}
+
+const auth=new Auth();
+export default auth;
