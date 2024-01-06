d5f683a2aaf3eb1ff57c75c8a3ecb102d3fe8f96
Vinaysohaliya <sohaliyavin01@gmail.com>
2024-01-06 14:55:36 +0530

    Search component updated

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from 'react';
 import userservice from '../Appwrite/user';
 import SearchCard from './SearchCard';
 import { IoMdClose } from "react-icons/io";
+import Loader from '../Components/Loader/Loader';
 
 const Search = ({ setSearchVisible }) => {
-    
+
     const [input, setInput] = useState("");
     const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
+                setIsLoading(true);
                 if (input.trim() === "") {
                     setUsers([]);
                 } else {
@@ -19,6 +22,8 @@ const Search = ({ setSearchVisible }) => {
                 }
             } catch (error) {
                 console.error(error);
+            } finally {
+                setIsLoading(false);
             }
         };
 
@@ -36,13 +41,13 @@ const Search = ({ setSearchVisible }) => {
     }
 
     return (
-        <div className='bg-black text-white w-full md:w-1/3 h-1/2 flex flex-col p-4 rounded-md'>
+        <div className=' bg-slate-50 text-black w-full md:w-1/3 h-1/2 flex flex-col p-4 rounded-md'>
             <div className='flex items-center justify-between mb-4'>
                 <div className='text-xl font-bold'>Search User</div>
                 <button
                     onClick={() => setSearchVisible(false)}
                 >
-                    <IoMdClose className='text-white' />
+                    <IoMdClose className='text-black' />
                 </button>
             </div>
             <div className='mb-2'>Username</div>
@@ -52,7 +57,13 @@ const Search = ({ setSearchVisible }) => {
                 onChange={handleChange}
             />
             <div>
-                {users && (
+                {isLoading && <div className=' flex h-full items-center justify-center'>
+                    <Loader />
+                </div>} 
+                {!isLoading && users.length === 0 && (
+                    <p className="text-center text-gray-500">No users found</p>
+                )}
+                {users.length > 0 && (
                     <div className='max-h-40 md:max-h-30 overflow-y-scroll'>
                         {users.map((user) => (
                             <SearchCard key={user.$id} name={user.name} profileId={user.profileId} userId={user.$id} />
